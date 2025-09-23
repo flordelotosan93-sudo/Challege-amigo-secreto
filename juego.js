cacda2cb7d2c6d847006c12baf7152ed8d52ec3c
flordelotosan93-sudo <flordelotosan93@gmail.com>
2025-09-23 17:46:29 -0500

    Update juego.js
    se agregan las posibles funciones que se van a necesitar

--- juego.js
+++ juego.js
@@ -1,1 +1,10 @@
-let 
+let listaDeAmigos = [];//Array que guarda los nombres de los amigos 
+
+//funcion para agregar un amigo
+
+
+
+//funcion para mostrar los amigos en la lista 
+
+//funcion para sortear un amigo secreto
+
