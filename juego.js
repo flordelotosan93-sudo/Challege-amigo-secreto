a3e7b2b27470a3e8488948b48be807d323789a1d
Liyet Arias <flordelotosa@gmail.com>
2025-09-23 20:29:27 -0500

    Actualizar challenge-amigo-secreto

--- juego.js
+++ juego.js
@@ -1,7 +1,9 @@
 let listaDeAmigos = [];//Array que guarda los nombres de los amigos 
 
 //funcion para agregar un amigo
-
+function agregarAmigo(){
+    const inputAmigo=document.getElementById("amigo");
+}
 
 
 //funcion para mostrar los amigos en la lista 
